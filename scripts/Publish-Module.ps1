d66b30c69f0dcb1d61530121124a968074c7e130
Maciek Misztal <mmisztal1980@gmail.com>
2022-10-02 15:35:25 +0200

    fix: syntax

--- scripts/Publish-Module.ps1
+++ scripts/Publish-Module.ps1
@@ -6,12 +6,11 @@ Param(
   [Parameter(Mandatory = $false)][string]$organization
 )
 
-$creds = New-Object System.Management.Automation.PSCredential -ArgumentList $username, (ConvertTo-SecureString -AsPlainText $password -Force);
-Register-PSRepository -Name $sourceName -SourceLocation $source -PublishLocation $source -Credential $creds;
-
 $source = "https://nuget.pkg.github.com/$organization/index.json"
-$module = '<module-name>'
-$version = '<module-version>'
+$creds = New-Object System.Management.Automation.PSCredential -ArgumentList $username, (ConvertTo-SecureString -AsPlainText $password -Force);
+Register-PSRepository -Name "GitHub" -SourceLocation $source -PublishLocation $source -Credential $creds;
+
+$version = '0.0.1'
 $apiKey = 'n/a' # keep this as n/a!
 
 Publish-Module -Name $module -Repository $sourceName -RequiredVersion $version -Credential $creds -Force -NuGetApiKey $apiKey
