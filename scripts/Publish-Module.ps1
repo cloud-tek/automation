10e832c7c44cc15131b1d156cfb27c3c74d54c90
Maciek Misztal <mmisztal1980@gmail.com>
2022-10-23 15:46:58 +0200

    chore: attempt to publish to nuget

--- scripts/Publish-Module.ps1
+++ scripts/Publish-Module.ps1
@@ -1,21 +1,21 @@
 [CmdletBinding()]
 Param(
   [Parameter(Mandatory = $true)][string]$module,
-  [Parameter(Mandatory = $true)][string]$username,
-  [Parameter(Mandatory = $true)][string]$password,
+  [Parameter(Mandatory = $true)][string]$url,
+  [Parameter(Mandatory = $true)][string]$apikey,
   [Parameter(Mandatory = $false)][string]$organization
 )
 
 Install-Module PowershellGet;
 Import-Module PowershellGet;
 
-$sourceName = "GitHub";
-$source = "https://nuget.pkg.github.com/$organization/index.json"
+$sourceName = "NuGet";
+$source = $url
 $creds = New-Object System.Management.Automation.PSCredential -ArgumentList $username, (ConvertTo-SecureString -AsPlainText $password -Force);
 Register-PSRepository -Name $sourceName -SourceLocation $source -PublishLocation $source -Credential $creds;
 
 $version = '0.0.1'
-$apiKey = 'n/a' # keep this as n/a!
+$apiKey = $apiKey; # 'n/a' # keep this as n/a!
 
 Write-Host "Publishing..."
 Publish-Module -Path "./$module" -Repository $sourceName -Credential $creds -Force -NuGetApiKey $apiKey -Verbose;
