5e35a331e0361123dac8cf1ec917e5cade90920a
Maciek Misztal <mmisztal1980@gmail.com>
2022-10-25 18:23:36 +0200

    chore: diag

--- src/CloudTek.Automation.K8S/PrePublish.ps1
+++ src/CloudTek.Automation.K8S/PrePublish.ps1
@@ -7,7 +7,8 @@
 Write-Host "Installing required modules..." -ForegroundColor Gray;
 
 $modules | % {
-  Write-Host "`t $_" -ForegroundColor Gray;
+  Write-Host "`t Registering PSRepository $_ ..." -ForegroundColor Gray;
+  Write-Host "`t Installing $_ ..." -ForegroundColor Gray;
   Register-PSRepository -Name $_ -SourceLocation "$PSScriptRoot/../$_";
   Install-Module $_ -Repository $_
 }
