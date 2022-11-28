ffcd78f406a1c4ffbcce72a882069dafb2084125
Maciek Misztal <mmisztal1980@gmail.com>
2022-11-28 21:39:47 +0100

    chore: reigster-psresourcerepository

--- src/CloudTek.Automation.K8S/PrePublish.ps1
+++ src/CloudTek.Automation.K8S/PrePublish.ps1
@@ -7,11 +7,14 @@
 $modules | % {
   Write-Host "Registering PSRepository ($_) ..." -ForegroundColor Gray;
   Write-Host "`t Installing $_ ..." -ForegroundColor Gray;
-  Register-PSRepository -Name $_ -SourceLocation "$PSScriptRoot/../$_";
+
+  #Register-PSRepository -Name $_ -SourceLocation "$PSScriptRoot/../$_";
+  Register-PSResourceRepository -Name "NuGetGallery" -Uri "https://api.nuget.org/v3/index.json" -Trusted
 
   Get-PSRepository;
 
   Get-Module -ListAvailable PowerShellGet,PackageManagement;
 
-  Install-Module $_ -Repository $_ -MinimumVersion "0.0.0" -Verbose -ErrorAction Break;
+  #Install-Module $_ -Repository $_ -MinimumVersion "0.0.0" -Verbose -ErrorAction Break;
+  Install-PSResource -Name "CloudTek.Automation.Shell" -Version "0.1.119" -Repository NuGetGallery -Verbose
 }
