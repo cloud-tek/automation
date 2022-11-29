9fbc0fa48e27a74d8420de038cf73e37dd1e8790
Maciek Misztal <mmisztal1980@gmail.com>
2022-11-29 01:04:05 +0100

    chore: fixes

--- src/CloudTek.Automation.K8S/PrePublish.ps1
+++ src/CloudTek.Automation.K8S/PrePublish.ps1
@@ -1,26 +1,10 @@
 #!/usr/local/bin/pwsh
 
+[string]$Repository = "nuget";
+
 [hashtable]$data = Import-PowerShellDataFile ./CloudTek.Automation.K8S.psd1
 
 $data.RequiredModules | % {
   Write-Host "`t Installing $($_.ModuleName) ($($_.ModuleVersion)) ..." -ForegroundColor Gray;
-  Install-PSResource -Name $_.ModuleName -Version $_.ModuleVersion -Repository NuGetGallery -Verbose
+  Install-PSResource -Name $_.ModuleName -Version $_.ModuleVersion -Repository $Repository -Verbose;
 }
-
-# [string[]]$modules = @(
-#   "CloudTek.Automation.Shell"
-# );
-
-# $modules | % {
-#   Write-Host "Registering PSRepository ($_) ..." -ForegroundColor Gray;
-#   Write-Host "`t Installing $_ ..." -ForegroundColor Gray;
-
-#   #Register-PSRepository -Name $_ -SourceLocation "$PSScriptRoot/../$_";
-
-#   Get-PSRepository;
-
-#   Get-Module -ListAvailable PowerShellGet,PackageManagement;
-
-#   #Install-Module $_ -Repository $_ -MinimumVersion "0.0.0" -Verbose -ErrorAction Break;
-#   Install-PSResource -Name "CloudTek.Automation.Shell" -Version "0.1.119" -Repository NuGetGallery -Verbose
-# }
