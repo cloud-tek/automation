49e7c830ccc2b0ed1f4183842393cb88ee58ce9c
Maciek Misztal <mmisztal1980@gmail.com>
2022-12-11 00:42:54 +0100

    fix: register

--- scripts/Test.ps1
+++ scripts/Test.ps1
@@ -1,19 +1,7 @@
 Import-Module -Name "$PSScriptRoot/Utils.psm1" -Force;
 Import-PowerShellGet -Version "3.0.17";
 
-function Register-LocalRepository() {
-  [string]$p = "$PSScriptRoot/../packages";
-
-  [hashtable]$arguments = @{
-    Name = "local"
-    SourceLocation = $p
-    PublishLocation = $p
-    InstallationPolicy = "Trusted"
-    ErrorAction = "SilentlyContinue"
-  };
-
-  Register-PSRepository @arguments;
-}
+Register-LocalRepository;
 
 Get-PsResourceRepository;
 
