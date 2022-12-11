e97efdb0eb5d20e37ad81ceb56c1b52f7d02c85a
Maciek Misztal <mmisztal1980@gmail.com>
2022-12-11 15:21:14 +0100

    chore: version

--- scripts/Publish.ps1
+++ scripts/Publish.ps1
@@ -27,7 +27,7 @@ try {
 
   # Write-Host "Publishing: $module ==($version)==> nuget ..." -ForegroundColor Gray;
 
-  Publish-PSResource -Path "$packages/$module.nupkg" `
+  Publish-PSResource -Path "$packages/$module.$version.nupkg" `
     -Repository "nuget" `
     -ApiKey $apiKey `
     -Verbose `
