7bd69d8da3c2bd032e0ef44ca4360e566e93b8ee
Maciek Misztal <mmisztal1980@gmail.com>
2022-12-12 01:40:41 +0100

    feat: argocd/get-argocdapplications

---
+++ src/CloudTek.Automation.ArgoCD/ArgoCD.psm1
@@ -0,0 +1,38 @@
+using module "CloudTek.Automation.Shell";
+function Get-ArgoCDApplications()
+{
+  [CmdLetBinding()]
+  [OutputType([string[]])]
+  param(
+    [Parameter(Mandatory = $false)][string]$kubeConfig,
+    [Parameter(Mandatory = $false)][string]$context,
+    [Parameter(Mandatory = $true)][string]$namespace
+  )
+  Set-StrictMode -Version Latest;
+  $ErrorActionPreference = "Stop";
+
+  [System.Collections.Generic.List[string]]$arguments = New-Object System.Collections.Generic.List[string];
+
+  $arguments.Add("--namespace $namespace");
+  $arguments.Add("get applications");
+
+  if (-not([string]::IsNullOrEmpty($kubeconfig))) {
+    $arguments.Add("--kubeconfig $kubeconfig");
+  }
+  if (-not([string]::IsNullOrEmpty($context))) {
+    $arguments.Add("--context $context");
+  }
+
+  [ShellExecutor]$shellExecutor = New-Object ShellExecutor -ArgumentList 'kubectl', $arguments;
+
+  $proc = $shellExecutor.Execute();
+
+  if ($proc.exitCode -ne 0)
+  {
+    Write-Error ("Error: kubectl apply exited with code: {0}" -f $exitCode);
+    Exit $proc.exitCode;
+  }
+
+}
+
+Export-ModuleMember -Function Get-ArgoCDApplications;
