5a8528b2b0a81b4a022b66694fe6b4524aa0a0eb
Maciek Misztal <mmisztal1980@gmail.com>
2023-01-08 15:32:53 +0100

    fix: tests

---
+++ src/CloudTek.Automation.Shell/Shell.psm1
@@ -0,0 +1,42 @@
+function Invoke-ShellCommand {
+  [CmdletBinding()]
+  [OutputType([int])]
+  param(
+    [Parameter(Mandatory = $true)][string]$Command,
+    [Parameter(Mandatory = $false)][string[]]$Arguments = $null,
+    [Parameter(Mandatory = $false)][scriptblock]$StandardOut = $null,
+    [Parameter(Mandatory = $false)][scriptblock]$StandardErr = $null
+  )
+
+  . {
+    [System.Diagnostics.ProcessStartInfo]$pInfo = New-Object System.Diagnostics.ProcessStartInfo;
+    $pInfo.FileName = $Command;
+    $pInfo.RedirectStandardError = $true;
+    $pInfo.RedirectStandardOutput = $true;
+    $pInfo.UseShellExecute = $false;
+    $pInfo.Arguments = $Arguments;
+
+    [System.Diagnostics.Process]$process = New-Object System.Diagnostics.Process
+    $process.StartInfo = $pInfo;
+    $process.Start();
+
+    $stdout = $process.StandardOutput.ReadToEnd();
+    $stderr = $process.StandardError.ReadToEnd();
+
+    $process.WaitForExit();
+
+    if ((0 -eq $process.ExitCode) -and ($null -ne $StandardOut)) {
+      $StandardOut.Invoke($stdout) | Out-Null;
+    }
+
+    if ((0 -ne $process.ExitCode) -and ($null -ne $StandardErr)) {
+      $StandardErr.Invoke($stderr) | Out-Null;
+    }
+
+    if (0 -ne $process.ExitCode) {
+      throw "Process exited with $($process.ExitCode)";
+    }
+  } | Out-Null;
+
+  return $process.ExitCode;
+}
--- src/CloudTek.Automation.Shell/Tests.ps1
+++ src/CloudTek.Automation.Shell/Tests.ps1
@@ -1,8 +1,38 @@
 Install-Module Pester;
 Import-Module Pester;
 
-Describe -Name "CloudTek.Automation.Shell Tests" {
-  It "should run" {
-    $true | Should -Be $true;
+Import-Module $PSScriptRoot/Shell.psm1;
+
+[bool]$stdoutHit = $false;
+
+Describe -Name "Invoke-ShellCommand Tests" {
+  It "Should execute basic OS command and process STDOUT output" {
+     if($IsLinux -or $IsMacOS) {
+      Invoke-ShellCommand -Command "ls" `
+        -StandardOut {
+          param($stdout)
+          Write-Host $stdout;
+          $script:stdoutHit = $true;
+        } `
+        -StandardErr {
+          param($stderr)
+          $false | Should -Be $true -Because "Should never reach this scriptblock";
+        } | Should -Be 0;
+    } elseif ($IsWindows) {
+      Invoke-ShellCommand -Command "dir" `
+        -StandardOut {
+          param($stdout)
+          Write-Host $stdout;
+          $script:stdoutHit = $true;
+        } `
+        -StandardErr {
+          param($stderr)
+          $false | Should -Be $true -Because "Should never reach this scriptblock";
+        } | Should -Be 0;
+    } else {
+      throw "Unsupported OS"
+    }
+
+    $stdoutHit | Should -BeTrue;
   }
 }
