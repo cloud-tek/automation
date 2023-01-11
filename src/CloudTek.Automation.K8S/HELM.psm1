b49cd94e1b2e11e903982706b0b1a31fda4b8ec1
Maciek Misztal <mmisztal1980@gmail.com>
2023-01-11 01:58:08 +0100

    HELM test

--- src/CloudTek.Automation.K8S/HELM.psm1
+++ src/CloudTek.Automation.K8S/HELM.psm1
@@ -1,155 +1,146 @@
 Set-StrictMode -Version Latest;
 $ErrorActionPreference = "Stop";
 
-[hashtable]$modules = @{
-  "CloudTek.Automation.Shell" = "Shell.psm1"
-  "CloudTek.Automation.Utilities" = "Utilities.psm1"
-};
+function Invoke-HelmUpgrade() {
+  [CmdLetBinding()]
+  param(
+    [Parameter(Mandatory = $true)][string]$chart,
+    [Parameter(Mandatory = $true)][string]$releaseName,
+    [Parameter(Mandatory = $true)][string]$version,
+    [Parameter(Mandatory = $false)][string]$kubeConfig,
+    [Parameter(Mandatory = $false)][string]$context,
+    [Parameter(Mandatory = $false)][string]$outputDir,
+    [Parameter(Mandatory = $false)][string]$namespace,
+    [Parameter(Mandatory = $false)][string]$timeout = "1200s",
+    [Parameter(Mandatory = $false)][string[]]$values,
+    [Parameter(Mandatory = $false)][hashtable]$overrides,
+    [Parameter(Mandatory = $false)][hashtable]$repositories,
+    [Parameter(Mandatory = $false)][switch]$atomic,
+    [Parameter(Mandatory = $false)][switch]$force,
+    [Parameter(Mandatory = $false)][switch]$dryRun
+  )
+  Get-Command -Cmd "helm";
 
-$modules.Keys | % {
-  if(Test-Path $PSScriptRoot/../$_/$modules[$_]) {
-    Import-Module $PSScriptRoot/../$_/$modules[$_] -Force;
-  } else {
-    Import-Module $_;
+  Write-Host "Rendering template...";
+
+  [hashtable]$repos = @{
+    "stable" = "https://charts.helm.sh/stable"
+  }
+
+  if (!($null -eq $repositories)) {
+    foreach ($key in $repositories.Keys) {
+      $repos.Add($key, $repositories[$key]);
+    }
+  }
+
+  foreach ($key in $repos.Keys) {
+    Write-Host "Adding $key repo...";
+    & helm repo add $key $repos[$key]
+  }
+
+  & helm repo update
+
+  [System.Collections.Generic.List[string]] $arguments = New-Object System.Collections.Generic.List[string];
+  if ($DebugPreference -ne "SilentlyContinue") {
+    $arguments.Add("--debug");
+  }
+
+  if (![string]::IsNullOrEmpty($outputDir)) {
+    if (!(Test-Path $outputDir)) {
+      New-Item -ItemType Directory -Path $outputDir;
+    }
+
+    $arguments.Add("template");
+    $arguments.Add("--output-dir $outputDir");
+  }
+  else {
+    if ($dryRun) {
+      $arguments.Add("upgrade --install --dry-run");
+    }
+    else {
+      $arguments.Add("upgrade --install");
+      $arguments.Add("--create-namespace");
+      $arguments.Add("--wait");
+      $arguments.Add("--timeout $timeout");
+    }
+
+    if(-not([string]::IsNullOrEmpty($kubeConfig))) {
+      $arguments.Add("--kubeconfig $kubeconfig");
+    }
+
+    if (-not([string]::IsNullOrEmpty($context))) {
+      $arguments.Add("--kube-context $context");
+    }
+
+    if ($atomic.IsPresent -and ($atomic -eq $true)) {
+      $arguments.Add("--atomic");
+    }
+
+    if ($force.IsPresent -and ($force -eq $true)) {
+      $arguments.Add("--force");
+    }
+  }
+
+  if (![string]::IsNullOrEmpty($namespace)) {
+    $arguments.Add("--namespace $namespace");
+  }
+
+  $arguments.Add("--version $version");
+
+  if ($null -ne $values) {
+    $values | % {
+      Write-Host "Including values: $_";
+      $arguments.Add("--values $_");
+    }
+  }
+
+  [System.Text.StringBuilder]$overrideArguments = New-Object System.Text.StringBuilder;
+  if (!($null -eq $overrides)) {
+    [int]$idx = 0;
+    foreach ($key in $overrides.Keys) {
+      Write-Host "Including overide: $key";
+      if (0 -eq $idx) {
+        $overrideArguments.Append("--set-string $key={0}" -f $overrides[$key]);
+      }
+      else {
+        $overrideArguments.Append(",$key={0}" -f $overrides[$key])
+      }
+      $idx++;
+    }
+
+    $arguments.Add($overrideArguments.ToString());
+  }
+
+  $arguments.Add($releaseName);
+  if ("." -eq $chart) {
+    $arguments.Add($MyInvocation.PSScriptRoot);
+  }
+  else {
+    $arguments.Add($chart);
+  }
+
+  [System.Text.StringBuilder]$sb = New-Object System.Text.StringBuilder;
+  $arguments | % {
+    $sb.Append("$_ ");
+  }
+
+  [int]$exitCode = Invoke-ShellCommand `
+    -Command "helm" `
+    -Arguments $arguments.ToArray() `
+    -StandardOut {
+      param($stdout)
+      Write-Host $stdout;
+    } `
+    -StandardErr {
+      param($stderr)
+      Write-Host $stderr;
+    };
+
+  if ($exitCode -ne 0) {
+    [string]$msg = "Error: helm exited with code: {0}" -f $exitCode;
+    Write-Error $msg;
+    throw $msg;
   }
 }
 
-function Deploy-HelmTemplate() {
-    [CmdLetBinding()]
-    param(
-        [Parameter(Mandatory = $true)][string]$kubeConfig,
-        [Parameter(Mandatory = $false)][string]$context,
-        [Parameter(Mandatory = $true)][string]$chart,
-        [Parameter(Mandatory = $true)][string]$releaseName,
-        [Parameter(Mandatory = $false)][string]$outputDir,
-        [Parameter(Mandatory = $false)][string]$namespace,
-        [Parameter(Mandatory = $true)][string]$version,
-        [Parameter(Mandatory = $false)][string]$timeout = "1200s",
-        [Parameter(Mandatory = $true)][string[]]$values,
-        [Parameter(Mandatory = $false)][hashtable]$overrides,
-        [Parameter(Mandatory = $false)][hashtable]$repositories,
-        [Parameter(Mandatory = $false)][switch]$atomic,
-        [Parameter(Mandatory = $false)][switch]$force,
-        [Parameter(Mandatory = $false)][switch]$dryRun
-    )
-    Get-Command -Cmd "helm";
-
-    Write-Host "Rendering template...";
-    if (($null -eq $values) -or ($values.Length -eq 0)) {
-        Write-Error "No values were provided";
-        return 1;
-    }
-
-    [hashtable]$repos = @{
-        "stable" = "https://charts.helm.sh/stable"
-    }
-
-    if (!($null -eq $repositories)) {
-        foreach ($key in $repositories.Keys) {
-            $repos.Add($key, $repositories[$key]);
-        }
-    }
-
-    foreach ($key in $repos.Keys) {
-        Write-Host "Adding $key repo...";
-        & helm repo add $key $repos[$key]
-    }
-
-    & helm repo update
-
-    [System.Collections.Generic.List[string]] $arguments = New-Object System.Collections.Generic.List[string];
-    if($DebugPreference -ne "SilentlyContinue"){
-        $arguments.Add("--debug");
-    }
-
-    if (![string]::IsNullOrEmpty($outputDir)) {
-        if (!(Test-Path $outputDir)) {
-            New-Item -ItemType Directory -Path $outputDir;
-        }
-
-        $arguments.Add("template");
-        $arguments.Add("--output-dir $outputDir");
-    }
-    else {
-        if($dryRun){
-          $arguments.Add("install --dry-run");
-        }else{
-          $arguments.Add("upgrade --install");
-          $arguments.Add("--create-namespace");
-          $arguments.Add("--wait");
-          $arguments.Add("--timeout $timeout");
-        }
-        $arguments.Add("--kubeconfig $kubeconfig");
-
-        if (-not([string]::IsNullOrEmpty($context))) {
-            $arguments.Add("--kube-context $context");
-        }
-
-        if($atomic.IsPresent -and ($atomic -eq $true)) {
-            $arguments.Add("--atomic");
-        }
-
-        if($force.IsPresent -and ($force -eq $true)) {
-            $arguments.Add("--force");
-        }
-    }
-
-    if(![string]::IsNullOrEmpty($namespace)){
-        $arguments.Add("--namespace $namespace");
-    }
-
-    $arguments.Add("--version $version");
-
-    $values | % {
-        Write-Host "Including values: $_";
-        $arguments.Add("--values $_");
-    }
-
-    [System.Text.StringBuilder]$overrideArguments = New-Object System.Text.StringBuilder;
-    if (!($null -eq $overrides)) {
-        [int]$idx = 0;
-        foreach ($key in $overrides.Keys) {
-            Write-Host "Including overide: $key";
-            if (0 -eq $idx) {
-                $overrideArguments.Append("--set-string $key={0}" -f $overrides[$key]);
-            }
-            else {
-                $overrideArguments.Append(",$key={0}" -f $overrides[$key])
-            }
-            $idx++;
-        }
-
-        $arguments.Add($overrideArguments.ToString());
-    }
-
-    $arguments.Add($releaseName);
-    if ("." -eq $chart) {
-        $arguments.Add($MyInvocation.PSScriptRoot);
-    }
-    else {
-        $arguments.Add($chart);
-    }
-
-    [System.Text.StringBuilder]$sb = New-Object System.Text.StringBuilder;
-    $arguments | % {
-        $sb.Append("$_ ");
-    }
-
-    Write-Host ("Executing: helm {0}" -f $sb.ToString()) -ForegroundColor Yellow;
-    Write-Host;
-
-    [int]$exitCode = Invoke-ShellCommand `
-      -Command `
-      -Arguments;
-    [ShellExecutor] $shellExecutor = New-Object ShellExecutor -ArgumentList 'helm', $arguments;
-
-    $proc = $shellExecutor.Execute();
-
-    if ($proc.exitCode -ne 0) {
-        Write-Error ("helm template rendering failed with exit code: {0}" -f $exitCode);
-    }
-
-    return $exitCode;
-}
-
-Export-ModuleMember -Function Deploy-HelmTemplate;
+Export-ModuleMember -Function Invoke-HelmUpgrade;
--- src/CloudTek.Automation.K8S/Tests.ps1
+++ src/CloudTek.Automation.K8S/Tests.ps1
@@ -1,10 +1,11 @@
-Install-Module Pester;
+Install-Module Pester -Force;
 Import-Module Pester;
 
 Import-Module $PSScriptRoot/../CloudTek.Automation.Shell/Shell.psm1 -Force;
 Import-Module $PSScriptRoot/../CloudTek.Automation.Utilities/Utilities.psm1 -Force;
 Import-Module $PSScriptRoot/Kubectl.psm1 -Force;
 Import-Module $PSScriptRoot/Kubeconform.psm1 -Force;
+Import-Module $PSScriptRoot/HELM.psm1 -Force;
 
 Describe -Name "CloudTek.Automation.K8S Kubectl Tests" {
   It "Command should be available" {
@@ -35,21 +36,21 @@ Describe -Name "CloudTek.Automation.K8S Kubeconform Tests" {
   }
 
   It "Should validate a valid deployment manifest" {
-   {
+    {
       Invoke-Kubeconform `
         -Path "$PSScriptRoot/tests/data/valid/deployment.yaml" `
         -Summary;
-   } | Should -Not -Throw;
+    } | Should -Not -Throw;
   }
 
   It "Should validate a valid deployment manifest in strict mode" {
     {
-       Invoke-Kubeconform `
-         -Path "$PSScriptRoot/tests/data/valid/deployment.yaml" `
-         -Strict `
-         -Summary;
+      Invoke-Kubeconform `
+        -Path "$PSScriptRoot/tests/data/valid/deployment.yaml" `
+        -Strict `
+        -Summary;
     } | Should -Not -Throw;
-   }
+  }
 
   It "Should not validate an invalid deployment manifest" {
     {
@@ -72,5 +73,22 @@ Describe -Name "CloudTek.Automation.K8S Kubeconform Tests" {
 Describe -Name "CloudTek.Automation.K8S HELM Tests" {
   It "Command should be available" {
     Get-Command -Cmd "helm" | Should -BeTrue;
+  }
+
+  It "Should upgrade from a public chart in dry-run mode"  -ForEach @(
+    @{ "Repository" = "jaegertracing";  "Address" = "https://jaegertracing.github.io/helm-charts";  "Chart" = "jaeger"; "Version" = "0.39.0"; "Release" = "jg";  }
+    @{ "Repository" = "dex";            "Address" = "https://charts.dexidp.io";                     "Chart" = "dex";    "Version" = "0.12.1"; "Release" = "dex"; }
+    @{ "Repository" = "kubereboot";     "Address" = "https://kubereboot.github.io/charts";          "Chart" = "kured";  "Version" = "4.2.0";  "Release" = "kured";  }
+  ) {
+    [hashtable]$repositories = @{
+      "$Repository" = "$Address"
+    };
+
+    Invoke-HelmUpgrade `
+      -Chart "$Repository/$Chart" `
+      -Release "$Release" `
+      -Version "$Version" `
+      -Repositories $repositories `
+      -DryRun;
   }
 }
